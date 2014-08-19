30da816798ae65334569bc5fca372d4ab208fc6c
Raul Andrade <raul.andrade@dce.ufpb.br>
2014-08-19 11:55:58 -0300

    Task 1.23 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/ManagerDrink.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/ManagerDrink.java
@@ -22,10 +22,10 @@ public class ManagerDrink {
 		if (!factory.getCupDispenser().contains(1)) {
 			factory.getDisplay().warn(Messages.OUT_OF_CUP);
 			return false;
-		} else if (!factory.getWaterDispenser().contains(1)) {
+		} else if (!factory.getWaterDispenser().contains(100)) {
 			factory.getDisplay().warn(Messages.OUT_OF_WATER);
 			return false;
-		} else if (!factory.getCoffeePowderDispenser().contains(1)) {
+		} else if (!factory.getCoffeePowderDispenser().contains(15)) {
 			factory.getDisplay().warn(Messages.OUT_OF_COFFEE_POWDER);
 			return false;
 		} else if (this.coffeeService.getDrink() == Drink.WHITE	|| this.coffeeService.getDrink() == Drink.WHITE_SUGAR) {
@@ -41,7 +41,7 @@ public class ManagerDrink {
 	public boolean checksSugar(ComponentsFactory factory) {
 
 		if (this.coffeeService.getDrink() == Drink.BLACK_SUGAR || this.coffeeService.getDrink() == Drink.WHITE_SUGAR) {
-			if (!factory.getSugarDispenser().contains(1)) {
+			if (!factory.getSugarDispenser().contains(200)) {
 				factory.getDisplay().warn(Messages.OUT_OF_SUGAR);
 				return false;
 			}
@@ -51,15 +51,15 @@ public class ManagerDrink {
 
 	public void mixingDrink(ComponentsFactory factory) {
 		factory.getDisplay().info(Messages.MIXING);
-		factory.getCoffeePowderDispenser().release(1);
-		factory.getWaterDispenser().release(3);
+		factory.getCoffeePowderDispenser().release(15);
+		factory.getWaterDispenser().release(100);
 	}
 
 	public void releaseDrink(ComponentsFactory factory) {
 		this.coffeeService.release();
 		factory.getDisplay().info(Messages.RELEASING);
 		factory.getCupDispenser().release(1);
-		factory.getDrinkDispenser().release(1);
+		factory.getDrinkDispenser().release(100);
 		factory.getDisplay().info(Messages.TAKE_DRINK);
 	}
 
